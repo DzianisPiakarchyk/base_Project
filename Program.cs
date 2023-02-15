80bdb754fb37515197e199a557c351cd1f81af76
Den <ff12den@gmail.com>
2023-02-15 19:07:37 +0300

    Написание кода программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string[] array = {"-ne", "two", "t, 6", "6ы", "4u,5r", "!ve", "sixx"};
+Console.Write($"[\"{String.Join("\", \"", array)}\"] --> ");
+string[] array2 = GetThreeElem(array);
+if (array2.Length == 0)
+{
+    Console.Write("[]");
+}
+else
+{
+    Console.Write($"[\"{String.Join("\", \"", array2)}\"]");
+}
+
+string[] GetThreeElem(string[] list) 
+    { 
+        int count = 0; 
+        for (int i = 0; i < list.Length; i++) 
+        { 
+            if (list[i].Length <= 3) count++; 
+        }
+        string[] result = new string[count];  
+        int j = 0;  
+        for (int i = 0; i < list.Length; i++)  
+        {  
+            if (list[i].Length <= 3)  
+            {  
+                result[j] = list[i];  
+                j++;  
+            }  
+        }  
+        return result; 
+    }    
